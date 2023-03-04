d61d27f6f2d7168db03089616ce7145bc7f69d6a
Clifford Thompson <clifford@thinkific.com>
2023-03-03 16:00:53 -0800

    master: Added some of the skeleton HTML, CSS, and JS.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// ***************************************************************************
+//                                                                           *
+//  Copyright (c) 2023 Clifford Thompson                                     *
+//                                                                           *
+//  All code in this file is released under Creative Commons Attribution     *
+//  (CC-BY) license : https://creativecommons.org/licenses/by/4.0/.          *
+//                                                                           *
+// ***************************************************************************
+
+
